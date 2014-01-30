5586111e7c3e1aa7312f9918ed609425c7ebea1a
VitalyTVA <vitalytva@yandex.ru>
2014-01-31 00:52:24 +0400

    t

---
+++ LazySharp/L.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazySharp {
+    public struct L<T> {
+        T value;
+        Func<T> func;
+
+        public T Value { 
+            get {
+                if(!HasValue) {
+                    value = func();
+                    func = null;
+                }
+                return value;
+            } 
+        }
+        public bool HasValue { get { return func != null; } }
+
+        public L(T value) {
+            this.value = value;
+            this.func = null;
+        }
+        public L(Func<T> func) {
+            this.value = default(T);
+            this.func = null;
+        }
+    }
+}
