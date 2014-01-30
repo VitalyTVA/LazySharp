01bfdbb271e48a4999ce1fd6a5dc99ebc6be9c9c
VitalyTVA <vitalytva@yandex.ru>
2014-01-30 23:05:35 +0400

    t

--- LazySharp.Roslyn/Program.cs
+++ LazySharp.Roslyn/Program.cs
@@ -9,7 +9,33 @@ using Roslyn.Services.CSharp;
 
 namespace LazySharp.Roslyn {
     class Program {
+        public const string CODE =
+@"using System;
+
+    namespace HelloWorld
+    {
+        public class TestClass
+        {
+            public string TestField;
+            public string TestProperty { get; set; }
+            public string TestMethod() { return null; }
+            public string TestMethod2(int k, string p) { return null; }
+            public TestClass ChainTest;
+        }
+    }";
+
+
         static void Main(string[] args) {
+            SyntaxTree tree = SyntaxTree.ParseText(CODE);
+            //SyntaxNode root = tree.GetRoot();
+
+            var compilation = Compilation.Create("test.dll")
+                .AddReferences(new MetadataFileReference(typeof(object).Assembly.Location))
+                .WithOptions(new CompilationOptions(OutputKind.DynamicallyLinkedLibrary))
+                .AddSyntaxTrees(tree);
+
+            var result = compilation.Emit("test.dll");
+
         }
     }
 }
