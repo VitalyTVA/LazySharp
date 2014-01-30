9750ed66882c9e9f42a23d5b9d99899f45c90a7e
VitalyTVA <vitalytva@yandex.ru>
2014-01-30 22:34:53 +0400

    Create

---
+++ LazySharp.Roslyn/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Roslyn.Compilers;
+using Roslyn.Compilers.CSharp;
+using Roslyn.Services;
+using Roslyn.Services.CSharp;
+
+namespace LazySharp.Roslyn {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
