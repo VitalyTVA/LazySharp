791a904ebf26c7283fe13b890c2cce075759b30c
VitalyTVA <vitalytva@yandex.ru>
2014-02-01 15:25:44 +0400

    t

--- LazySharp/Tests/LListTests.cs
+++ LazySharp/Tests/LListTests.cs
@@ -41,25 +41,38 @@ namespace LazySharp.Tests {
                 callCount2++;
                 return 13;
             };
-            var list = new LList<int>(f1.MakeLazy(), new LList<int>(f2.MakeLazy()).AsLazy()).AsLazy();
+
+            int listCallCount2 = 0;
+            Func<LList<int>> fList2 = () => {
+                listCallCount2++;
+                return new LList<int>(f2.MakeLazy());
+            };
+
+            int listCallCount1 = 0;
+            Func<LList<int>> fList1 = () => {
+                listCallCount1++;
+                return new LList<int>(f1.MakeLazy(), fList2.MakeLazy());
+            };
+
+            var list = fList1.MakeLazy();
             IEnumerator<int> en = list.AsEnumerable().GetEnumerator();
-            callCount1.IsEqual(0);
-            callCount2.IsEqual(0);
+            callCount1.IsEqual(0); callCount2.IsEqual(0); listCallCount1.Equals(0); listCallCount2.Equals(0);
             en.MoveNext().IsTrue();
-            callCount1.IsEqual(1);
-            callCount2.IsEqual(0);
+            callCount1.IsEqual(1); callCount2.IsEqual(0); listCallCount1.Equals(0); listCallCount2.Equals(0);
             en.Current.IsEqual(9);
-            callCount1.IsEqual(1);
-            callCount2.IsEqual(0);
+            callCount1.IsEqual(1); callCount2.IsEqual(0); listCallCount1.Equals(1); listCallCount2.Equals(0);
 
             en.MoveNext().IsTrue();
-            callCount1.IsEqual(1);
-            callCount2.IsEqual(1);
+            callCount1.IsEqual(1); callCount2.IsEqual(1); listCallCount1.Equals(1); listCallCount2.Equals(0);
             en.Current.IsEqual(13);
-            callCount1.IsEqual(1);
-            callCount2.IsEqual(1);
+            callCount1.IsEqual(1); callCount2.IsEqual(1); listCallCount1.Equals(1); listCallCount2.Equals(1);
 
             en.MoveNext().IsFalse();
+
+            callCount1.IsEqual(1);
+            callCount2.IsEqual(1);
+            listCallCount1.Equals(1);
+            listCallCount2.Equals(1);
         }
     }
 }
