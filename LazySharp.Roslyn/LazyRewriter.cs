365b98579d542e266b2855ed67904a0018fa2dcc
VitalyTVA <vitalytva@yandex.ru>
2014-02-04 02:09:03 +0400

    t

--- LazySharp.Roslyn/LazyRewriter.cs
+++ LazySharp.Roslyn/LazyRewriter.cs
@@ -18,14 +18,21 @@ using Roslyn.Services.CSharp;
 namespace LazySharp.Roslyn {
     class LazyRewriter : SyntaxRewriter {
         public override SyntaxNode VisitNamespaceDeclaration(NamespaceDeclarationSyntax node) {
+            //var newName = (NameSyntax)new NamespaceRewriter().Visit(node.Name);
+            //var newNode = node.WithName(newName);
+
+            //var newName = Syntax.QualifiedName(Syntax.IdentifierName("LazySharp"), Syntax.IdentifierName("Generated")).WithTrailingTrivia(node.Name.GetTrailingTrivia());
+            //var newNode = node.WithName(newName);
+
             QualifiedNameSyntax namespaceSyntax = Syntax.QualifiedName(Syntax.IdentifierName("LazySharp"), Syntax.IdentifierName("Generated")).WithTrailingTrivia(node.Name.GetTrailingTrivia());
-            return Syntax.NamespaceDeclaration(namespaceSyntax.WithLeadingTrivia(node.NamespaceKeyword.TrailingTrivia), node.Externs, node.Usings, node.Members)
+            var newNode = Syntax.NamespaceDeclaration(namespaceSyntax.WithLeadingTrivia(node.NamespaceKeyword.TrailingTrivia), node.Externs, node.Usings, node.Members)
                 .WithOpenBraceToken(node.OpenBraceToken);
+            return base.VisitNamespaceDeclaration(newNode);
         }
         //public override SyntaxNode VisitParameter(ParameterSyntax node) {
         //    var trail = node.Type.GetTrailingTrivia().Single();
         //    var clearType = node.Type.ReplaceTrivia(trail, SyntaxTriviaList.Empty);
-        //    var newType = Syntax.GenericName("L").AddTypeArgumentListArguments(clearType).WithTrailingTrivia(trail);
+        //    var newType = Syntax.GenericName("L").AddTypeArgumentListArguments(clearType).WithTrailingTrivia(node.Type.GetTrailingTrivia());
         //    return node.WithType(newType);
         //}
         //public override SyntaxNode VisitBinaryExpression(BinaryExpressionSyntax node) {
@@ -33,5 +40,12 @@ namespace LazySharp.Roslyn {
         //    var syntaxArgumentList = Syntax.ArgumentList(syntaxSeparatedList);
         //    return Syntax.InvocationExpression(Syntax.MemberAccessExpression(SyntaxKind.MemberAccessExpression, node.Left, Syntax.IdentifierName("Add")), syntaxArgumentList);
         //}
+    }
+    class NamespaceRewriter : SyntaxRewriter {
+        public override SyntaxNode VisitIdentifierName(IdentifierNameSyntax node) {
+            if((string)node.Identifier.Value == "Prototypes")
+                node = node.WithIdentifier(Syntax.Identifier("Generated").WithLeadingTrivia(node.Identifier.LeadingTrivia).WithTrailingTrivia(node.Identifier.TrailingTrivia));
+            return base.VisitIdentifierName(node);
+        }
     }
 }
